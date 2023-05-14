76e39bf729a865aa88acabe6646da2cab2597d59
unknown <vlad.vtorygin@gmail.com>
2023-05-14 10:45:34 +0300

    fix specification

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -3,7 +3,11 @@ const HttpError = require("../helpers/HttpError");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
+    console.log(error.message);
     if (error) {
+      if (Object.keys(req.body).length === 0) {
+        throw HttpError(400, "missing fields");
+      }
       next(HttpError(400, error.message));
     }
     next();
