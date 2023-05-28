d04c68e6d52f713d69ecd2cbf3792862ca7ab78e
unknown <vlad.vtorygin@gmail.com>
2023-05-28 12:44:39 +0300

    Complete

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const contactsController = require("../../controllers/contact-controllers");
-
-const { contactsAddSchema } = require("../../schemas/contacts-schema");
+const isValidId = require("../../middlewares/isValidId");
+const {
+  contactsAddSchema,
+  updateFavoriteSchema,
+} = require("../../models/contact");
 const validateBody = require("../../decorators/validateBody");
+const validFavoriteBody = require("../../middlewares/validFavorite");
 
 const router = express.Router();
 
 router.get("/", contactsController.getAllContacts);
 
-router.get("/:contactId", contactsController.getContactById);
+router.get("/:contactId", isValidId, contactsController.getContactById);
 
 router.post(
   "/",
@@ -16,12 +20,20 @@ router.post(
   contactsController.addContact
 );
 
-router.delete("/:contactId", contactsController.deleteContact);
+router.delete("/:contactId", isValidId, contactsController.deleteContact);
 
 router.put(
   "/:contactId",
+  isValidId,
   validateBody(contactsAddSchema),
   contactsController.updateContactById
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validFavoriteBody(updateFavoriteSchema),
+  contactsController.updateFavoriteContact
 );
 
 module.exports = router;
